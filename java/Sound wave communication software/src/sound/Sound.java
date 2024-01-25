32120b7891491a6bb0680e4dd19e63eac47b9962
soysaucecrab <88924237+soysaucecrab@users.noreply.github.com>
2024-01-26 02:34:46 +0900

    x

--- java/Sound wave communication software/src/sound/Sound.java
+++ java/Sound wave communication software/src/sound/Sound.java
@@ -1,40 +1,109 @@
 package sound;
 
+import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Graphics;
 import java.awt.Image;
-import java.util.Observer;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Sound extends JFrame {
-	
+
 	private Image screenImage;
 	private Graphics screenGraphic;
+
+	private ImageIcon exitButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/Menu_Exit_press.png"));
+	private ImageIcon exitButtonBasicImage = new ImageIcon(Main.class.getResource("../images/Menu_Exit.png"));
 	
-	private Image introBackground;
+	private Image introBackground = new ImageIcon(Main.class.getResource("../images/intro_Background_2.jpg")).getImage();
+	private JLabel menuBar = new JLabel(new ImageIcon(Main.class.getResource("../images/Menu_Bar.png")));
 	
-	public Sound() { 
+	private JButton exitButton = new JButton(exitButtonBasicImage);
+	
+	private int mouseX, mouseY;
+	
+	public Sound() {
+		setUndecorated(true);
 		setTitle("Sound wave communication software");
-		setSize(Main.SCREEN_WIDTH,Main.SCREEN_HEIGHT);
+		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
 		setResizable(false);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
+		setBackground(new Color(0, 0, 0, 0));
+		setLayout(null);
+
+		menuBar.setBounds(0, 0, 1280, 30);
+		menuBar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				mouseX = e.getX();
+				mouseY = e.getY();
+			}
+		});
+		menuBar.addMouseMotionListener(new MouseAdapter() {
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				int x = e.getXOnScreen();
+				int y = e.getYOnScreen();
+				setLocation(x - mouseX, y - mouseY);
+			}
+		});
 		
-		introBackground = new ImageIcon(Main.class.getResource("../images/intro_Background_1.jpg")).getImage();
+		
+		exitButton.setBounds(1250, 1, 30, 30);
+		exitButton.setBorderPainted(false);
+		exitButton.setContentAreaFilled(false);
+		exitButton.setFocusPainted(false);
+		exitButton.addMouseListener(new MouseAdapter(){
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				exitButton.setIcon(exitButtonEnteredImage);
+				exitButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				exitButton.setIcon(exitButtonBasicImage);
+				exitButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				System.exit(0);
+			}
+		});
+		
+		
+		//add
+		
+		
+		
+		//input
+
+		
+		//music
+//		Music introMusic = new Music("introMusic.mp3", true);
+//		introMusic.start();
+		
+		add(exitButton);
+		add(menuBar);
 	}
-	
+
 	public void paint(Graphics g) {
-		screenImage = createImage(Main.SCREEN_WIDTH,Main.SCREEN_HEIGHT);
+		screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
 		screenGraphic = screenImage.getGraphics();
 		screenDraw(screenGraphic);
 		g.drawImage(screenImage, 0, 0, null);
 	}
-	
+
 	public void screenDraw(Graphics g) {
 		g.drawImage(introBackground, 0, 0, null);
+		paintComponents(g);
 		this.repaint();
 	}
-	
+
 }
