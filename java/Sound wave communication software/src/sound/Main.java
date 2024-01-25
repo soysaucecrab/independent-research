a32e975fea0621d67c82a7151177b30395529849
soysaucecrab <88924237+soysaucecrab@users.noreply.github.com>
2024-01-26 00:20:20 +0900

    s

--- java/Sound wave communication software/src/sound/Main.java
+++ java/Sound wave communication software/src/sound/Main.java
@@ -10,11 +10,10 @@ public class Main {
 	public static final int SCREEN_HEIGHT = 720;
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Audio audio = new Audio();
 		
 		new Sound();
 		
-		audio.play();
+		
 	}
 }
--- java/Sound wave communication software/src/sound/Sound.java
+++ java/Sound wave communication software/src/sound/Sound.java
@@ -1,8 +1,18 @@
 package sound;
 
+import java.awt.Graphics;
+import java.awt.Image;
+import java.util.Observer;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class Sound extends JFrame {
+	
+	private Image screenImage;
+	private Graphics screenGraphic;
+	
+	private Image introBackground;
 	
 	public Sound() { 
 		setTitle("Sound wave communication software");
@@ -11,5 +21,20 @@ public class Sound extends JFrame {
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
+		
+		introBackground = new ImageIcon(Main.class.getResource("../images/intro_Background_1.jpg")).getImage();
 	}
+	
+	public void paint(Graphics g) {
+		screenImage = createImage(Main.SCREEN_WIDTH,Main.SCREEN_HEIGHT);
+		screenGraphic = screenImage.getGraphics();
+		screenDraw(screenGraphic);
+		g.drawImage(screenImage, 0, 0, null);
+	}
+	
+	public void screenDraw(Graphics g) {
+		g.drawImage(introBackground, 0, 0, null);
+		this.repaint();
+	}
+	
 }
